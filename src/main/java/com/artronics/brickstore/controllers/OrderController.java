25664f2c629853cfbb41bd15d89184418fb72634
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-11-24 11:23:27 +0000

    add status test and if customer does not exist it return 404

--- src/main/java/com/artronics/brickstore/controllers/OrderController.java
+++ src/main/java/com/artronics/brickstore/controllers/OrderController.java
@@ -1,12 +1,17 @@
 package com.artronics.brickstore.controllers;
 
+import com.artronics.brickstore.entities.Customer;
+import com.artronics.brickstore.entities.Order;
 import com.artronics.brickstore.repositories.CustomerRepository;
 import com.artronics.brickstore.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/customers/{customerId}/orders")
@@ -21,12 +26,22 @@ public class OrderController {
     }
 
     @GetMapping(path = "/{orderId}")
-    public void getOrder(@PathVariable Long orderId) {
+    public ResponseEntity<Order> getOrder(@PathVariable Long customerId, @PathVariable Long orderId) {
+        Customer customer = customerRepository.findOne(customerId);
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
 
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public void getOrders() {
+    public ResponseEntity<List<Order>> getOrders(@PathVariable Long customerId) {
+        Customer customer = customerRepository.findOne(customerId);
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
 
+        return ResponseEntity.ok().build();
     }
 }
--- src/test/java/com/artronics/brickstore/controllers/GetOrderTest.java
+++ src/test/java/com/artronics/brickstore/controllers/GetOrderTest.java
@@ -1,5 +1,6 @@
 package com.artronics.brickstore.controllers;
 
+import com.artronics.brickstore.entities.Customer;
 import com.artronics.brickstore.repositories.CustomerRepository;
 import com.artronics.brickstore.repositories.OrderRepository;
 import org.junit.Before;
@@ -9,6 +10,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class GetOrderTest {
     protected MockMvc mockMvc;
@@ -21,24 +26,44 @@ public class GetOrderTest {
     @Mock
     protected CustomerRepository customerRepository;
 
+    Customer customer;
+
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(orderController).build();
+
+        customer = new Customer(1L, "John");
+
+        // findOne(1) always returns customer, if not we redefine it inside test
+        when(customerRepository.findOne(1L)).thenReturn(customer);
     }
 
     @Test
     public void it_should_send_ok_status_for_single_order() throws Exception {
-
+        mockMvc.perform(get("/customers/1/orders/123"))
+                .andExpect(status().isOk());
     }
 
     @Test
     public void it_should_send_ok_status_for_all_orders() throws Exception {
-
+        mockMvc.perform(get("/customers/1/orders/123"))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void with_GET_it_returns_404_if_customer_does_not_exist() throws Exception {
+    public void with_GET_single_it_returns_404_if_customer_does_not_exist() throws Exception {
+        when(customerRepository.findOne(1L)).thenReturn(null);
+        // then
+        mockMvc.perform(get("/customers/1/orders/123"))
+                .andExpect(status().isNotFound());
+    }
 
+    @Test
+    public void with_GET_list_resource_it_returns_404_if_customer_does_not_exist() throws Exception {
+        when(customerRepository.findOne(1L)).thenReturn(null);
+        // then
+        mockMvc.perform(get("/customers/1/orders"))
+                .andExpect(status().isNotFound());
     }
 }
