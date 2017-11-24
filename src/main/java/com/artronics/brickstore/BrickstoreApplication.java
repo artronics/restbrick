75f01eb1da226223d625c8c54e3ecf6602fbe499
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-11-24 10:24:58 +0000

    Initial Commit

---
+++ src/main/java/com/artronics/brickstore/BrickstoreApplication.java
@@ -0,0 +1,12 @@
+package com.artronics.brickstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickstoreApplication.class, args);
+	}
+}
